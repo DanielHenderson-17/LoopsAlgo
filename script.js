05b25e6211ba14e0e87e740410022d8760fddbf6
Daniel Henderson <djhindu17@gmail.com>
2020-11-04 00:57:48 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Print odds 1-20
+for(let i=0; i<20; i++){
+    if(i % 2 !== 0)
+    console.log(i);
+}
+
+//Sum and Print 1-5
+var sum=0;
+for(let i=0; i<5; i++){
+    sum+=i
+    console.log('num:', i, 'sum:', sum)
+}
